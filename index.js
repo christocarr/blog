6dfef3483e428c32cb72683c6f7af4e64c5b0ea3
christocarr <christocarr@outlook.com>
2019-04-04 13:22:17 +0100

    Add register user schema

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const indexRoutes = require('./routes/index');
 
 dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true })
+  .then(() => console.log('MONGODB connected'))
+  .catch((err) => console.log(err))
 
 const app = express();
 
