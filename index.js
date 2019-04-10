b342ec34a444cd9b4236fdc2eaadb41ae135ea42
Chris Carr <christocarr@outlook.com>
2019-04-10 20:10:41 +0100

    Add static public

--- index.js
+++ index.js
@@ -16,6 +16,8 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
+app.use(express.static('public'));
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(flash());
