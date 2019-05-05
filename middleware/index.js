9062b380b636ad406cdfa5da06dc2e8debac8ebe
Chris Carr <christocarr@outlook.com>
2019-05-05 15:29:24 +0100

    Add user authen on edit post

--- middleware/index.js
+++ middleware/index.js
@@ -1,8 +1,37 @@
+const Post = require('../models/Post');
+
 exports.isLoggedIn = (req, res, next) => {
   if (req.isAuthenticated()) {
     next()
   } else {
     req.flash('error', 'You need to be logged in!');
     res.redirect('/login');
+  }
+}
+
+//check ownership
+exports.isOwnerPost = async (req, res, next) => {
+  if (req.isAuthenticated()) {
+    try {
+      //find post by id
+      const post = await Post.findById(req.params.id);
+      if (!post) {
+        req.flash('error', 'Post not found');
+        return res.redirect('back');
+      } else {
+        if (req.user._id === post.userId) {
+          next();
+        } else {
+          req.flash('error', `You don't have permission`);
+          return res.redirect('back');
+        }
+      }
+    } catch (err) {
+        req.flash('error', 'Something went wrong');
+        return res.redirect('back');
+    }
+  } else {
+      req.flash('error', 'You must be logged in');
+      res.redirect('back');
   }
 }
